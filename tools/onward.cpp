0efb96efc0988ab0ca44ac8d6aa32e8eb4615a15
Alex Cameron <asc@tetsuo.sh>
2021-12-19 11:36:01 +1100

    interpreter: Add interpreter prompt

--- tools/onward.cpp
+++ tools/onward.cpp
@@ -2,12 +2,18 @@
 
 #include <iostream>
 
+namespace {
+
+const char *gOnwardPrompt = "ONWARD> ";
+
+} // namespace
+
 int main(int argc, char **argv) {
   static_cast<void>(argc);
   static_cast<void>(argv);
   onward::Engine engine;
   std::string line;
-  while (std::cin >> line) {
+  while (std::cout << gOnwardPrompt, std::getline(std::cin, line)) {
     engine.ExecWords(line);
   }
   return 0;
